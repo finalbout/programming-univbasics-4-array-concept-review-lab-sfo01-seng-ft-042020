7dbdb0c82f2db3ca9772a6ff562d9381432c5c20
John Tom Jr <johntjr.22@gmail.com>
2020-03-06 05:16:50 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,12 +11,9 @@ end
 
 def find_max_value(array)
   x = 0
-  array.length.times { |index|
-  if array[index] > x 
-    x = array[index]
-end
-}
-x
+  array.length.times{ |index| x = array[index]
+  if array[index] >}
+    x
 end
 
 def find_min_value(array)
