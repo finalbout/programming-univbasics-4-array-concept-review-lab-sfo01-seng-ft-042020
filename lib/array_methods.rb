e351cfa0e916919298edbd035d9e32e10151a958
John Tom Jr <johntjr.22@gmail.com>
2020-03-06 05:21:15 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -17,6 +17,10 @@ end
 
 def find_min_value(array)
   x = 0
-  array.length.times{ |index| x = array[index] if array[index] < x }
-  x
+  array.length.times do |index|
+    if array[index] < x
+      x = array[index]
+    end
+  end
+  x 
 end
